902de890f34f3107491bfa649c832e89f4ed207a
Diego Pineda <pinedamontenegrodiegoalejandro@gmail.com>
2026-02-06 08:39:22 -0500

    Se agrega la suma.

--- matematicas.js
+++ matematicas.js
@@ -1,4 +1,12 @@
 console.log("Hola, este es el sistema matematico inicial");
 
 
-let opcion = prompt("Ingrese 1 para sumas \n Ingrese 2 para multiplicar")
+let opcion = prompt("Ingrese 1 para sumas \n Ingrese 2 para restar \n Ingrese 3 para multiplicar \n Ingrese 4 para dividir")
+let numero1 = parseFloat(prompt("Ingrese el primer numero"))
+let numero2 = parseFloat(prompt("Ingrese el segundo numero"))
+let resultado;
+
+if (opcion == "1"){
+    resultado = numero1 + numero2
+    console.log("El resultado de la suma es: " + resultado)
+}
